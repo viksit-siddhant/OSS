22a55c9b98a0ba52167d167bd74a1915c3f04b3a
viksit-siddhant <43901482+viksit-siddhant@users.noreply.github.com>
2022-07-28 19:05:28 +0530

    New start, new me

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 import docker
 import re,os,json,tempfile,datetime, sys
 from git import Repo
@@ -9,19 +10,17 @@ for line in open('keys.txt','r'):
     key,value = line.strip().split(':')
     keys[key] = value
 
+g = Github(keys['github'])
+
 def get_url_of_package(package, platform):
-    url = f"https://libraries.io/api/{platform}/{package}"
-    cache = open('cache.txt','r')
-    for entry in cache:
-        if entry.split(':')[0] == url:
-            return entry.split(':')[1]
-    cache.close()
-    cache = open('cache.txt','a')
-    r = requests.get(f"{url}?api_key={keys['libraries']}")
-    destination = r.json()['repository_url']    
-    cache.write(f"{url}:{destination}\n")
-    cache.close()
-    return destination
+    if platform == "NPM":
+        url = f"https://www.npmjs.com/package/{package}"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        return soup.find_all('a', {'aria-labelledby':'repository'})[0].get('href')
+    elif platform == 'Pypi':
+        repos = g.search_repositories(f'{package} language:python')
+        return repos[0].html_url
 
 def cve_detection(url,client):
     raw = client.containers.run('aquasec/trivy',f'repo -f json {url}').decode('utf-8')
@@ -32,43 +31,13 @@ def cve_detection(url,client):
 def scan_files(repo):
     tempdir = tempfile.TemporaryDirectory()
     Repo.clone_from(repo, tempdir.name)
-    packages = []
-    try:
-        with open(os.path.join(tempdir.name, 'package.json'), 'r') as f:
-            for package in json.load(f)['dependencies']:
-                packages.append((package,'NPM'))
-    except FileNotFoundError:
-        pass
     urls = []
-    builtins = set([module.replace('_',"") for module in sys.builtin_module_names])
     for root, dirs, files in os.walk(tempdir.name):
         for file in files:
             try:
                 urls += re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+',open(os.path.join(root,file),'r').read())
-                if file.endswith('.py'):
-                    for line in open(os.path.join(root,file), "r"):
-                        if "import" in line:
-                            if "from" in line:
-                               packages.append((line.split("from")[1].split("import")[0].strip(),'Pypi'))
-                            else:
-                                lists, final = re.findall("import (.*),*(.*)",line)[0]
-
-                                for lib in lists.replace(' ','').split(","):
-                                    packages.append((lib.strip(),'Pypi'))
-                                if final:
-                                    packages.append((final.strip(),'Pypi'))
             except (FileNotFoundError,UnicodeDecodeError):
                 pass
-    dep_score = 9999999999999
-    packages = list(set(packages))
-    for package, platform in packages:
-        if package not in builtins:
-            try:
-                url = get_url_of_package(package, platform)
-                repo_analysis = metadata_analysis(url)
-                dep_score = min(dep_score,repo_analysis['authorScore'])
-            except KeyError:
-                dep_score -= 50
     urls = list(set(urls))
     body = {
     "client": {
@@ -92,10 +61,9 @@ def scan_files(repo):
             num_vuln_urls += len(r.json()['matches'])
 
     tempdir.cleanup()
-    return num_vuln_urls, dep_score
+    return num_vuln_urls
 
 def metadata_analysis(repo):
-    g = Github(keys['github'])
     repo = g.get_repo('/'.join(repo.split('/')[-2:]))
     stars = repo.stargazers_count
     num_issues = repo.open_issues_count
@@ -118,20 +86,14 @@ def metadata_analysis(repo):
             pass
     authors_score = 0
     for author in authors:
-        for repo in author.get_repos():
-            authors_score += repo.stargazers_count
-            for topic in repo.get_topics():
-                if topic in blacklist and topic not in whitelist:
-                    authors_score -= repo.stargazers_count
-                    authors_score -= author.followers
-                    break
         authors_score += author.followers
     score = authors_score * 0.3 + stars - num_issues*10 - (datetime.datetime.now() - last_committed).days / 10 - topic_sensitive * 100
-    return score
+    return score, topic_sensitive
 
 def main(url, logfile=None):
     if logfile is None:
         logfile = open('logs.log','w')
+    print("Running Trivy...")
     client = docker.from_env()
     cves = json.loads(cve_detection(url,client))
     severity = {}
@@ -148,12 +110,12 @@ def main(url, logfile=None):
                     severity[vuln['Severity']] += 1
     except KeyError:
         pass
-    num_trash_urls, dep_score = scan_files(url)
-    repo_score = metadata_analysis(url)
-    print(repo_score,dep_score)
-    print(num_trash_urls)
-    print(severity)
+    print("Scanning files and github")
+    num_trash_urls = scan_files(url)
+    repo_score, topics = metadata_analysis(url)
+    
 
 
 if __name__ == '__main__':
-    main('https://github.com/encryptedcation/NoDoubt')
+   main('https://github.com/encryptedcation/NoDoubt')
+   #print(get_url_of_package('discord','Pypi'))
